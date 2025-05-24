5c4ad95c3a4a34482bc3316514ba2179c78146c3
szabi_qqq <kucserka.szabolcs@hallgato.sze.hu>
2025-05-25 01:55:38 +0200

    asd

--- include/ros2_pong_semiautomatic/game_manager.hpp
+++ include/ros2_pong_semiautomatic/game_manager.hpp
@@ -2,6 +2,7 @@
 #define ROS2_PONG_SEMIAUTOMATIC__GAME_MANAGER_HPP_
 
 #include "rclcpp/rclcpp.hpp"
+#include "ros2_pong_semiautomatic/msg/game_state.hpp"
 
 class GameManager : public rclcpp::Node
 {
@@ -12,6 +13,7 @@ private:
   void update_game();
 
   rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<ros2_pong_semiautomatic::msg::GameState>::SharedPtr state_pub_;
 
   float ball_x_;
   float ball_y_;
--- src/game_manager.cpp
+++ src/game_manager.cpp
@@ -1,4 +1,8 @@
 #include "ros2_pong_semiautomatic/game_manager.hpp"
+#include "ros2_pong_semiautomatic/msg/game_state.hpp"
+
+using std::placeholders::_1;
+using ros2_pong_semiautomatic::msg::GameState;
 
 GameManager::GameManager()
 : Node("game_manager"),
@@ -13,23 +17,31 @@ GameManager::GameManager()
     std::chrono::milliseconds(50),  // 20 Hz
     std::bind(&GameManager::update_game, this)
   );
+
+  state_pub_ = this->create_publisher<GameState>("game_state", 10);
 }
 
 void GameManager::update_game()
 {
-  // egyszerű mozgás: csak halad a labda
+  // Labda mozgás frissítés
   ball_x_ += ball_vx_;
   ball_y_ += ball_vy_;
 
-  RCLCPP_INFO(this->get_logger(), "Ball position: [%.2f, %.2f]", ball_x_, ball_y_);
-
-  // egyszerű "fal" visszapattanás logika
+  // Visszapattanás
   if (ball_x_ > 1.0 || ball_x_ < -1.0) {
     ball_vx_ *= -1.0;
   }
   if (ball_y_ > 1.0 || ball_y_ < -1.0) {
     ball_vy_ *= -1.0;
   }
+
+  // Üzenet publikálása
+  GameState state_msg;
+  state_msg.ball_x = ball_x_;
+  state_msg.ball_y = ball_y_;
+  state_pub_->publish(state_msg);
+
+  RCLCPP_INFO(this->get_logger(), "Labda: [%.2f, %.2f]", ball_x_, ball_y_);
 }
 
 int main(int argc, char * argv[])
