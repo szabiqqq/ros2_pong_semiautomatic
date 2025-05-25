ff3283a50935b4121e6993648e16a8c603e6807d
szabi_qqq <kucserka.szabolcs@hallgato.sze.hu>
2025-05-25 10:39:12 +0200

    nem tudom már

--- src/game_manager.cpp
+++ src/game_manager.cpp
@@ -1,20 +1,16 @@
 #include "ros2_pong_semiautomatic/game_manager.hpp"
-#include "ros2_pong_semiautomatic/msg/game_state.hpp"
 
-using std::placeholders::_1;
 using ros2_pong_semiautomatic::msg::GameState;
 
 GameManager::GameManager()
 : Node("game_manager"),
-  ball_x_(0.0),
-  ball_y_(0.0),
-  ball_vx_(0.01),  // vízszintes sebesség
-  ball_vy_(0.01)   // függőleges sebesség
+  ball_x_(0.0), ball_y_(0.0),
+  ball_vx_(0.01), ball_vy_(0.01)
 {
   RCLCPP_INFO(this->get_logger(), "GameManager node elindult!");
 
   timer_ = this->create_wall_timer(
-    std::chrono::milliseconds(50),  // 20 Hz
+    std::chrono::milliseconds(50),
     std::bind(&GameManager::update_game, this)
   );
 
@@ -23,19 +19,12 @@ GameManager::GameManager()
 
 void GameManager::update_game()
 {
-  // Labda mozgás frissítés
   ball_x_ += ball_vx_;
   ball_y_ += ball_vy_;
 
-  // Visszapattanás
-  if (ball_x_ > 1.0 || ball_x_ < -1.0) {
-    ball_vx_ *= -1.0;
-  }
-  if (ball_y_ > 1.0 || ball_y_ < -1.0) {
-    ball_vy_ *= -1.0;
-  }
+  if (ball_x_ > 1.0 || ball_x_ < -1.0) ball_vx_ *= -1.0;
+  if (ball_y_ > 1.0 || ball_y_ < -1.0) ball_vy_ *= -1.0;
 
-  // Üzenet publikálása
   GameState state_msg;
   state_msg.ball_x = ball_x_;
   state_msg.ball_y = ball_y_;
